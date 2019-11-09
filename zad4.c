f7d4c490a111b8305eb5974ce5c9792708ca167b
presnelfesb <56390312+presnelfesb@users.noreply.github.com>
2019-11-09 18:39:42 +0100

    Update and rename zad4_2.c to zad4.c

---
+++ zad4.c
@@ -0,0 +1,214 @@
+#define CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+struct polinom;
+typedef struct polinom Polinom;
+typedef struct polinom *position;
+
+
+struct polinom {
+	int koef;
+	int expo;
+	position next;
+};
+
+int Ispis(position);
+int Stvori(position*);
+int Unos(position);
+int Zbroji(position, position, position);
+int Umnozak(position, position, position);
+
+
+int main()
+{
+	int n = 0;
+	Polinom p1, p2, zbroj, umnozak;
+
+	p1.next = NULL;
+	printf("Prvi polinom\n");
+	Unos(&p1);
+	Ispis(&p1);
+	
+	p2.next = NULL;
+	printf("\nDrugi polinom\n");
+	Unos(&p2);
+	Ispis(&p2);
+
+	zbroj.next = NULL;
+	printf("\nZbroj polinoma je:\n");
+	Zbroji(&p1, &p2, &zbroj);
+	Ispis(&zbroj);
+
+	umnozak.next = NULL; 
+	printf("\nUmnozak polinoma je:\n");
+	Umnozak(&p1, &p2, &umnozak);
+	Ispis(&umnozak);
+
+
+
+
+	return 0;
+}
+
+int Unos(position p)
+{
+	int n = 0;
+	int i = 0;
+	position q = NULL, temp=NULL;
+
+
+	printf("Koliko cete unjeti clanova u polinom:"); scanf("%d", &n);
+	
+	printf("Unosi koeficijente i eksponencije:");
+
+	temp = p;
+	for (i = 0; i < n; i++)
+	{
+		Stvori(&q);
+		scanf(" %d %d", &q->koef, &q->expo);
+
+		while (p->next != NULL && q->expo < p->next->expo)
+			p = p->next;
+
+		if (p->next != NULL && q->expo == p->next->expo  )
+		{
+			p->next->koef = p->next->koef + q->koef;
+			free(q);
+		}
+		else
+		{
+			q->next = p->next;
+			p->next = q;
+		}
+		p = temp;
+		
+	}
+	return 0;
+}
+
+int Stvori(position *p)
+{
+	position q = NULL;
+	*p = (position)malloc( sizeof(Polinom));
+	return 0;
+
+}
+
+int Ispis(position p)
+{
+	p = p->next;
+	printf("(%dx^%d)",p->koef,p->expo);
+	p = p->next;
+	while (p != NULL)
+	{
+		printf("+(%dx^%d)", p->koef, p->expo);
+		p = p->next;
+	}
+	return 0;
+}
+
+int Zbroji(position p1, position p2, position z)
+{
+	position q = NULL, temp = NULL;
+
+	p1 = p1->next;
+	p2 = p2->next;
+
+	while (p1 != NULL && p2 != NULL)
+	{
+		Stvori(&q);
+		if (p1->expo > p2->expo)
+		{
+			q->koef = p1->koef;
+			q->expo = p1->expo;
+			p1 = p1->next;
+		}
+		else if (p2->expo > p1->expo)
+		{
+			q->koef = p2->koef;
+			q->expo = p2->expo;
+			p2 = p2->next;
+		}
+		else
+		{
+			q->koef = p1->koef+p2->koef;
+			q->expo = p1->expo;
+			p1 = p1->next;
+			p2 = p2->next;
+		}
+		q->next = z->next;
+		z->next = q;
+		z = q;
+	}
+
+	if (p1 != NULL)
+		temp = p1;
+	else
+		temp = p2;
+
+	while (temp != NULL)
+	{
+		Stvori(&q);
+		q->koef = temp->koef;
+		q->expo = temp->expo;
+	
+		q->next = z->next;
+		z->next = q;
+		z = q;
+
+		temp = temp->next;
+	}
+
+	return 0;
+}
+
+int Umnozak(position p1, position p2, position u)
+{
+	position q = NULL, temp1=NULL;
+	position temp2=NULL;
+	p1 = p1->next;
+	p2 = p2->next;
+
+	temp2 = u;
+	temp1 = p2;
+	
+
+	while (p1 != NULL)
+	{
+		while (p2 != NULL)
+		{
+			Stvori(&q);
+			q->koef = p1->koef * p2->koef;
+			q->expo = p1->expo + p2->expo;
+
+			u = temp2;
+			while (u->next != NULL && u->next->expo > q->expo)
+				u = u->next;
+			
+			if (u->next!= NULL && q->expo == u->next->expo)
+			{
+				u->next->expo =  q->expo;
+				u->next->koef = u->next->koef + q->koef;
+
+			}
+			else
+			{
+				q->next = u->next;
+				u->next = q;
+
+				
+			}
+
+
+			p2 = p2->next;
+		}
+		p1 = p1->next;
+		p2 = temp1;
+	}
+	return 0;
+
+}
