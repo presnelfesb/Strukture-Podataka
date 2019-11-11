115a23190ac90bb98d1536fbe08f97410a2f7453
presnelfesb <56390312+presnelfesb@users.noreply.github.com>
2019-11-11 18:03:43 +0100

    Update zad4.c

--- zad4.c
+++ zad4.c
@@ -16,7 +16,6 @@ struct polinom {
 };
 
 int Ispis(position);
-int Stvori(position*);
 int Unos(position);
 int Zbroji(position, position, position);
 int Umnozak(position, position, position);
@@ -31,7 +30,7 @@ int main()
 	printf("Prvi polinom\n");
 	Unos(&p1);
 	Ispis(&p1);
-	
+
 	p2.next = NULL;
 	printf("\nDrugi polinom\n");
 	Unos(&p2);
@@ -42,7 +41,7 @@ int main()
 	Zbroji(&p1, &p2, &zbroj);
 	Ispis(&zbroj);
 
-	umnozak.next = NULL; 
+	umnozak.next = NULL;
 	printf("\nUmnozak polinoma je:\n");
 	Umnozak(&p1, &p2, &umnozak);
 	Ispis(&umnozak);
@@ -57,23 +56,25 @@ int Unos(position p)
 {
 	int n = 0;
 	int i = 0;
-	position q = NULL, temp=NULL;
+	position q = NULL, temp = NULL;
 
 
 	printf("Koliko cete unjeti clanova u polinom:"); scanf("%d", &n);
-	
-	printf("Unosi koeficijente i eksponencije:");
 
 	temp = p;
 	for (i = 0; i < n; i++)
 	{
-		Stvori(&q);
-		scanf(" %d %d", &q->koef, &q->expo);
+		q = (position)malloc(sizeof(Polinom));
+
+		printf("Unesi %d. koeficijent: ",i+1);
+		scanf(" %d", &q->koef);
+		printf("\Unesi %d. eksponent: ",i+1);
+		scanf(" %d", &q->expo);
 
 		while (p->next != NULL && q->expo < p->next->expo)
 			p = p->next;
 
-		if (p->next != NULL && q->expo == p->next->expo  )
+		if (p->next != NULL && q->expo == p->next->expo)
 		{
 			p->next->koef = p->next->koef + q->koef;
 			free(q);
@@ -84,28 +85,23 @@ int Unos(position p)
 			p->next = q;
 		}
 		p = temp;
-		
+
 	}
 	return 0;
 }
 
-int Stvori(position *p)
-{
-	*p = (position)malloc( sizeof(Polinom));
-	return 0;
-
-}
 
 int Ispis(position p)
 {
 	p = p->next;
-	printf("(%dx^%d)",p->koef,p->expo);
+	printf("(%dx^%d)", p->koef, p->expo);
 	p = p->next;
 	while (p != NULL)
 	{
 		printf("+(%dx^%d)", p->koef, p->expo);
 		p = p->next;
 	}
+	printf("\n");
 	return 0;
 }
 
@@ -118,7 +114,8 @@ int Zbroji(position p1, position p2, position z)
 
 	while (p1 != NULL && p2 != NULL)
 	{
-		Stvori(&q);
+		q = (position)malloc(sizeof(Polinom));
+
 		if (p1->expo > p2->expo)
 		{
 			q->koef = p1->koef;
@@ -133,7 +130,7 @@ int Zbroji(position p1, position p2, position z)
 		}
 		else
 		{
-			q->koef = p1->koef+p2->koef;
+			q->koef = p1->koef + p2->koef;
 			q->expo = p1->expo;
 			p1 = p1->next;
 			p2 = p2->next;
@@ -150,10 +147,10 @@ int Zbroji(position p1, position p2, position z)
 
 	while (temp != NULL)
 	{
-		Stvori(&q);
+		q = (position)malloc(sizeof(Polinom));
 		q->koef = temp->koef;
 		q->expo = temp->expo;
-	
+
 		q->next = z->next;
 		z->next = q;
 		z = z->next;
@@ -166,30 +163,32 @@ int Zbroji(position p1, position p2, position z)
 
 int Umnozak(position p1, position p2, position u)
 {
-	position q = NULL, temp1=NULL;
-	position temp2=NULL;
+	position q = NULL, temp1 = NULL;
+	position temp2 = NULL;
 	p1 = p1->next;
 	p2 = p2->next;
 
 	temp2 = u;
 	temp1 = p2;
-	
+
 
 	while (p1 != NULL)
 	{
 		while (p2 != NULL)
 		{
-			Stvori(&q);
+			u = temp2;
+			
+			q = (position)malloc(sizeof(Polinom));
 			q->koef = p1->koef * p2->koef;
 			q->expo = p1->expo + p2->expo;
 
-			u = temp2;
+			
 			while (u->next != NULL && u->next->expo > q->expo)
 				u = u->next;
-			
-			if (u->next!= NULL && q->expo == u->next->expo)
+
+			if (u->next != NULL && q->expo == u->next->expo)
 			{
-				u->next->expo =  q->expo;
+				u->next->expo = q->expo;
 				u->next->koef = u->next->koef + q->koef;
 
 			}
@@ -198,7 +197,7 @@ int Umnozak(position p1, position p2, position u)
 				q->next = u->next;
 				u->next = q;
 
-				
+
 			}
 
 
