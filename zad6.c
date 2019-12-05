30bb7c29a1c64b8d19c9e7c5cfd8b810641b765d
presnelfesb <56390312+presnelfesb@users.noreply.github.com>
2019-12-05 20:27:27 +0100

    Update zad6.c

--- zad6.c
+++ zad6.c
@@ -14,9 +14,11 @@ struct stog {
 };
 
 int push(position);
+int pushr(position);
 int pop(position);
 int opcije();
 int odabir(position);
+int pick();
 
 int main()
 {
@@ -25,35 +27,19 @@ int main()
 
 	odabir(&s);
 
-	
 
+	return 0;
 }
 
-int opcije()
-{
-	int x = 0;
-	
-	while (x!=3)
-	{
 
-		printf("Odaberite opciju koju zelite izabrati:\n ");
-		printf("1.Push\n");
-		printf("2.Pop\n");
-		printf("3.Izlaz\n");
-
-		scanf("%d", &x);
-		if (x == 0 || x > 3 || x < 1)
-			printf("KRIVI BROJ\n\n");
-		
-	}
-	return x;
-}
 
 int odabir(position p)
 {
 	int x = 0;
 	int a = 0;
 
+	a = pick();
+	
 	while (x != 3)
 	{
 		x = opcije();
@@ -66,15 +52,52 @@ int odabir(position p)
 			break;
 		}
 		case 1: {
-			printf("Unesi clan koji zelis staviti na stog\n");
-			scanf("%d", &a);
-			push(&p);
+			if(a==1)
+				push(&p);
+			else {
+				pushr(&p);
+			}
 			break;
 		}
 		}
 	}
 	return 0;
 
+}
+
+int pick()
+{
+	int x = 0;
+	while (x != 1 && x != 2)
+	{
+		printf("Izaberite:\n 1.Stog \n 2.Red\n\n");
+		scanf("%d", &x);
+
+		if (x < 1 || x>2)
+			printf("KRIVI ODABIR\n");
+	}
+
+	return x;
+}
+
+int opcije()
+{
+	int x = 0;
+
+
+
+
+	printf("Odaberite opciju koju zelite izabrati:\n ");
+	printf("1.Push\n");
+	printf("2.Pop\n");
+	printf("3.Izlaz\n");
+
+	scanf("%d", &x);
+	if (x == 0 || x > 3 || x < 1)
+		printf("KRIVI BROJ\n\n");
+
+
+	return x;
 }
 
 int push(position p)
@@ -84,24 +107,48 @@ int push(position p)
 	srand(time(0));
 
 	x = rand() % 90 + 10;
-	
+
 	q = (position)malloc(sizeof(Stog));
+	q->next = NULL;
 	q->next = p->next;
 	p->next = q;
-	p->el =x ;
+	q->el = x;
 
 	return 0;
 }
+
+int pushr(position p)
+{
+	position q = NULL;
+	int x = 0;
+	srand(time(0));
+
+	x = rand() % 90 + 10;
+
+	while (p->next != NULL)
+		p = p->next;
+
+	q = (position)malloc(sizeof(Stog));
+	q->next = p->next;
+	p->next = q;
+	q->el = x;
+
+	return 0;
+}
+
 
 int pop(position p)
 {
 	position temp = NULL;
 
+	temp= (position)malloc(sizeof(Stog));
+	temp->next = NULL;
+	
 	temp = p->next;
 	if (temp != NULL)
 	{
 		p->next = temp->next;
-		printf("%d", temp->el);
+		printf(" Pop-an clan je %d\n", temp->el);
 
 		free(temp);
 	}
